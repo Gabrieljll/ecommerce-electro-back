fb3eab9455622b9cc2f8bff8af77c9c5d6053747
Gabrieljll <48096093+Gabrieljll@users.noreply.github.com>
2024-03-03 04:00:20 -0300

    db data from env

--- db.js
+++ db.js
@@ -1,10 +1,20 @@
 import { createPool } from "mysql2/promise"
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+
+const HOST = process.env.DB_HOST || 'localhost'
+const PORT = process.env.DB_PORT
+const USER = process.env.DB_USER
+const PASSWORD = process.env.DB_PASSWORD
+const DATABASE = process.env.DB_DATABASE
 
 export const pool = createPool({
-  host: '',
-  port: 3306,
-  user: '',
-  password: '',
-  database: ''
+  host: HOST,
+  port: PORT,
+  user: USER,
+  password: PASSWORD,
+  database: DATABASE
 
 })
